157f4da77caeea59379e3812c9a321a3da4f6bab
wachirasak-p <60335216+wachirasak-p@users.noreply.github.com>
2023-10-19 22:28:33 +0700

    update 14

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,14 @@ export default async function Home() {
     <>
       <AuthButtonServer />
       <NewTweet />
-      <pre>{JSON.stringify(tweets, null, 2)}</pre>
+      {tweets?.map((tweet) => (
+        <div key={tweet.id}>
+          <p>
+            {tweet?.profiles?.name} {tweet?.profiles?.username}
+          </p>
+          <p>{tweet.title}</p>
+        </div>
+      ))}
     </>
   );
 }
